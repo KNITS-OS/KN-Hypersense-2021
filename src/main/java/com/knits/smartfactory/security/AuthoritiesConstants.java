5e24409f01f4e1f7f5bf9b07cd2483ac8baf9e49
Dmitri Marinen <dmitri.marinen@gmail.com>
2021-06-07 22:11:01 +0300

    update authorities to match Core

--- src/main/java/com/knits/smartfactory/security/AuthoritiesConstants.java
+++ src/main/java/com/knits/smartfactory/security/AuthoritiesConstants.java
@@ -9,6 +9,15 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    /**Authorities to be used while creating roles.
+     * Example 1: Admin will have all four authorities.
+     * Example 2: Analytic will have only PERMISSION_READ.
+     */
+    public static final String PERMISSION_CREATE = "ROLE_PERMISSION_CREATE";
+    public static final String PERMISSION_UPDATE = "ROLE_PERMISSION_UPDATE";
+    public static final String PERMISSION_READ = "ROLE_PERMISSION_READ";
+    public static final String PERMISSION_DELETE = "ROLE_PERMISSION_DELETE";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
